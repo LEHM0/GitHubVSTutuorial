9c43e8d4c4ffa241b2f8c3665ff88dcaa60da54b
LEHM0 <kennykawaii00@gmail.com>
2024-09-20 13:47:58 -0500

    Update Program.cs
    Console.WriteLine modified

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Me! This is how I use GH with VS!");
+            Console.WriteLine("Hello, Me! This is how I use GH with VS! And using VS through GH!");
         }
     }
 }
